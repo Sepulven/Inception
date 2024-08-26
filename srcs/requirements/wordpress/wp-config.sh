25f1432de28ac96b7ada37e8b62a3a9ed6ba7b00
Arthur Aguiar <sepulven40028922@gmail.com>
2024-08-26 22:07:33 +0100

    Volumes are not configured. And the wp-config.sh script has been updated, working as expected.

--- srcs/requirements/wordpress/wp-config.sh
+++ srcs/requirements/wordpress/wp-config.sh
@@ -3,67 +3,71 @@
 
 dir_path="/var/www/html/asepulve.42.fr/"
 file_name="wp-config.php"
-if [-d "$dir_path"] && [-f "$dir_path$file_name"]; then
- echo "All files have been created inside the volume. ";
- exit 0
+
+if ! [ -f "$dir_path$file_name" ]; then
+	cd /var/www/html
+
+	# --silent -> doesn't print anything to the terminal
+	# -O -> tells curl to save the file as it is named remotely
+	curl --silent -O https://wordpress.org/latest.tar.gz .
+
+	echo "Wordpress files have downloaded"
+
+	# -x -> extract file
+	# -z -> .gz files
+	# -f -> path to the file
+	# Extract file
+	tar -xzf latest.tar.gz
+
+	# Remove .tar.gz
+	rm latest.tar.gz
+
+	# Saves the files in a given directory
+
+	mkdir -p asepulve.42.fr
+
+	mv wordpress/* asepulve.42.fr
+
+	rm -rf wordpress
+
+	echo "Wordpress files discompressed and in folder"
+
+	cd asepulve.42.fr
+
+	# We will replace the database values with ours
+	# Comes from the .env file
+	sed -i "s/database_name_here/$MYSQL_DATABASE_NAME/" wp-config-sample.php
+	sed -i "s/username_here/$MYSQL_USERNAME/" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_LOCALHOST/" wp-config-sample.php
+
+	mv wp-config-sample.php wp-config.php
+
+	echo "Add database config inside the wp-config-sample.php. "
+
+	#sed - stream editor for filtering and transforming text 
+	# sed [OPTION]... {script-only-if-no-other-script} [input-file]
+	# -i -> changes the actual file
+	# "s/old/new" -> the script. It searchs in the file for the value and replace it
+	#It works the same way as vim searching pattern.
+
+
+	# Creates the folder for php
+	mkdir -p /run/php
+
+	# The /run/php directory is a temporary filesystem location used by PHP and its associated processes, like PHP-FPM (FastCGI Process Manager), to store runtime data. Here’s a closer look at its purpose:
+
+
 fi
-
-cd /var/www/html
-
-# --silent -> doesn't print anything to the terminal
-# -O -> tells curl to save the file as it is named remotely
-curl --silent -O https://wordpress.org/latest.tar.gz .
-
-echo "Wordpress files have downloaded"
-
-
-# -x -> extract file
-# -z -> .gz files
-# -f -> path to the file
-# Extract file
-tar -xzf latest.tar.gz
-
-# Remove .tar.gz
-rm latest.tar.gz
-
-# Saves the files in a given directory
-
-mkdir asepulve.42.fr
-
-mv wordpress/* asepulve.42.fr
-
-rm -rf wordpress
-
-
-echo "Wordpress files discompressed and in folder"
-
-
-cd asepulve.42.fr
-
-# We will replace the database values with ours
-# Comes from the .env file
-sed -i "s/database_name_here/$MYSQL_DATABASE_NAME" wp-config-sample.php
-sed -i "s/username_here/$MYSQL_USERNAME" wp-config-sample.php
-sed -i "s/password_here/$MYSQL_PASSWORD" wp-config-sample.php
-sed -i "s/localhost/$MYSQL_LOCALHOST" wp-config-sample.php
-
-mv wp-config-sample.php wp-config.php
-
-echo "Add database config inside the wp-config-sample.php. "
-
-#sed - stream editor for filtering and transforming text 
-# sed [OPTION]... {script-only-if-no-other-script} [input-file]
-# -i -> changes the actual file
-# "s/old/new" -> the script. It searchs in the file for the value and replace it
-#It works the same way as vim searching pattern.
-
-
-# Creates the folder for php
-mkdir -p /run/php
-
-# The /run/php directory is a temporary filesystem location used by PHP and its associated processes, like PHP-FPM (FastCGI Process Manager), to store runtime data. Here’s a closer look at its purpose:
-
 
 # Looks for the attr. listen inside the www.conf and changes it our port
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir -p /run/php
+touch /run/php/php7.4-fpm.pid
+
+#chown -R nginx:nginx /var/www/html
+
+echo "Starting php-fpm7.4 in the foreground[...]";
+/usr/sbin/php-fpm7.4 -F -R
 
