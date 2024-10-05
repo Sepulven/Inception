cdfb9b687301fdc30735c27f1a60ab2eb8cc79cd
Arthur Aguiar <sepulven40028922@gmail.com>
2024-10-05 19:46:40 +0100

    Add new Dockerfile for the static_website bonus. Add new node-config.sh for the static_website bonus.

---
+++ srcs/requirements/bonus/static_website/node-config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Directory in which we will have the start-upper folder
+
+if ! [ -f "/home/start_upper/package.json" ];
+then
+
+	mkdir /home/start_upper
+	# $TOKEN -> is the enviroment variable used to access all of my private repositories,
+	# it is from where we are going to clone the website
+	git clone "https://$GITHUB_TOKEN@github.com/sepulven/start-upper-pages_deprecated" "/home/start_upper"
+
+	npm install
+	npm install --save-dev nodemon
+else
+	echo "Start-Upper is already installed"
+fi
+
+
+
+cd /home/start_upper
+
+# Install all of the dependencies in the packake.json file.
+npm install
+
+# Builds all of the tailwind dependencies.
+npx tailwindcss -i statics/input.css -o statics/styles/style.css
+
+npm run start # Runs nodemon in the foreground.
