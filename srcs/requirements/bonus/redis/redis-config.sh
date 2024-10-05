04fb9bab46607d88f87ddfb4c5527654ef78a4c5
Arthur Aguiar <sepulven40028922@gmail.com>
2024-10-06 00:46:01 +0100

    Add  flags to the redis-server command and its respective documentations.
    I will begin to develop the integration of the redis container with wordpress.

--- srcs/requirements/bonus/redis/redis-config.sh
+++ srcs/requirements/bonus/redis/redis-config.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-redis-server
+# Link to this flag documentation: https://redis.io/docs/latest/operate/oss_and_stack/management/persistence/#aof-advantages
+# --appendonly yes -> AOF (Append Only File): AOF persistence logs every write operation received by the server. These operations can then be replayed again at server startup, reconstructing the original dataset. Commands are logged using the same format as the Redis protocol itself.
+
+# Link to the protected mode: https://redis.io/docs/latest/operate/oss_and_stack/management/security/#protected-mode
+# As we are working in a internal network and in developing enviroment we can turn the protected-mode off.
+redis-server --port 6379 --appendonly yes --protected-mode no
