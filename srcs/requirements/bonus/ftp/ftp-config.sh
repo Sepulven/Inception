4df4b784bc07358b84d6d777814fbdd050c9bd4b
Arthur Aguiar <sepulven40028922@gmail.com>
2024-10-08 15:27:51 +0100

    Add new information for the .env file. Add new configuration for the ftp server, still working on it.

--- srcs/requirements/bonus/ftp/ftp-config.sh
+++ srcs/requirements/bonus/ftp/ftp-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-if [ ! -f "/etc/vsftpd.conf.bak"];
-then
+if [ ! -f "/etc/vsftpd.conf.bak" ]; then
 
 	# Creates a backup of the default configuration file
 	# We might need this for backup in the future
@@ -17,37 +16,61 @@ then
 	# listen=YES -> the server will respond to Ipv4 
 	# background=NO -> forces the vsftp server to run in the foreground
 
-	export __FTP_CONFIG="listen=YES" \
-						"background=NO"\
-						"listen_ipv6=YES"\
-						"anonymous_enable=NO"\
-						"local_enable=YES"\
-						"write_enable=YES"\
-						"local_umask=022"\
-						"dirmessage_enable=YES"\
-						"use_localtime=YES"\
-						"xferlog_enable=YES"\
-						"connect_from_port_20=YES"\
-						"chroot_local_user=YES"\
-						"secure_chroot_dir=/var/run/vsftpd/empty"\
-						"pam_service_name=vsftpd"\
-						"pasv_enable=Yes"\
-						"pasv_min_port=10000"\
-						"pasv_max_port=11000"\
-						"user_sub_token=$USER"\
-						"local_root=/home/$USER/ftp"\
-						"userlist_enable=YES"\
-						"userlist_file=/etc/vsftpduserlist.conf"\
-						"userlist_deny=NO"
+	export __FTP_CONFIG= "listen=YES\n\
+						background=NO\n\
+						listen_ipv6=NO\n\
+						anonymous_enable=NO\n\
+						local_enable=YES\n\
+						write_enable=YES\n\
+						local_umask=022\n\
+						dirmessage_enable=YES\n\
+						use_localtime=YES\n\
+						xferlog_enable=YES\n\
+						connect_from_port_20=YES\n\
+						chroot_local_user=YES\n\
+						secure_chroot_dir=/var/run/vsftpd/empty\n\
+						pam_service_name=vsftpd\n\
+						pasv_enable=Yes\n\
+						pasv_min_port=10000\n\
+						pasv_max_port=11000\n\
+						user_sub_token=$USER\n\
+						local_root=/home/$USER/ftp\n\
+						userlist_enable=YES\n\
+						userlist_file=/etc/vsftpduserlist.conf\n\
+						userlist_deny=NO"
 
 	# We are going to be appending this content to the /etc/vsftp.conf
-	echo $__FTP_CONFIG >> /etc/vsftpd.conf
+	echo -e "listen=YES\n\
+			  background=NO\n\
+			  listen_ipv6=NO\n\
+			  anonymous_enable=NO\n\
+			  local_enable=YES\n\
+			  write_enable=YES\n\
+			  local_umask=022\n\
+			  dirmessage_enable=YES\n\
+			  use_localtime=YES\n\
+			  xferlog_enable=YES\n\
+			  connect_from_port_20=YES\n\
+			  chroot_local_user=YES\n\
+			  secure_chroot_dir=/var/run/vsftpd/empty\n\
+			  pam_service_name=vsftpd\n\
+			  pasv_enable=Yes\n\
+			  pasv_min_port=10000\n\
+			  pasv_max_port=11000\n\
+			  user_sub_token=$USER\n\
+			  local_root=/home/$USER/ftp\n\
+			  userlist_enable=YES\n\
+			  userlist_file=/etc/vsftpduserlist.conf\n\
+			  userlist_deny=NO" >> /etc/vsftpd.conf
 
-    echo $FTP_USER_NAME| tee -a /etc/vsftpd.userlist &> /dev/null
+	echo $FTP_USER_NAME| tee -a /etc/vsftpd.userlist > /dev/null 2>&1
+	echo "vsftp has been configured!"
 else
 	echo "FTP server has been configured!"
 fi
 
-
 echo "Starting ftp server on port 21..."
-vsftp /etc/vsftpd.conf
+vsftpd /etc/vsftpd.conf -o debug
+echo "We could not start the ftp server"
+
+tail -f
