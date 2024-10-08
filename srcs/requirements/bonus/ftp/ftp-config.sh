011143e71d1178e05bf3ec883833b5d60236ae37
Arthur Aguiar <sepulven40028922@gmail.com>
2024-10-08 21:31:20 +0100

    Rewrite script with a better approach.

--- srcs/requirements/bonus/ftp/ftp-config.sh
+++ srcs/requirements/bonus/ftp/ftp-config.sh
@@ -12,6 +12,10 @@ if [ ! -f "/etc/vsftpd.conf.bak" ]; then
 	mkdir -p /var/run/vsftpd/empty
 
 	echo $FTP_USER_NAME| tee -a /etc/vsftpd.userlist > /dev/null 2>&1
+	#ufw allow 21100:21110/tcp
+	# Grant the permission to the user
+	chown -R $FTP_USER_NAME:$FTP_USER_NAME /var/www/html/
+	chmod 755 /var/www/html/
 	echo "vsftp has been configured!"
 else
 	echo "FTP server has been configured!"
